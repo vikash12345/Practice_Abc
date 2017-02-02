c6da8a58ceda9c972f6044ba56f0aac157909d2c
vikash12345 <vikash@backgroundcheck.email>
2017-02-02 16:09:54 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,11 +1,12 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+ html = scraperwiki.scrape("http://python.org/downloads")
+ print("html")
 #
 # # Find something on the page using css selectors
 # root = lxml.html.fromstring(html)
